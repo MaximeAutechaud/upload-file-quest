45200ebec9e9d5b34063a7245e797391310eeb90
Maxime Autechaud <maxime.autechaud@gmail.com>
2020-11-08 18:17:43 +0100

    ok

--- public/upload.php
+++ public/upload.php
@@ -1,28 +1,56 @@
 <?php
 
-$errors = [];
+$it = new FilesystemIterator(__DIR__ . '/uploads');
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_FILES['avatar']['name'])) {
-    $type = mime_content_type($_FILES['avatar']['tmp_name']);
-    $extension = explode('/', $type);
-    if ($extension[1] === "jpeg" || $extension[1] === "png" || $extension[1] === "gif") {
-        $filename = uniqid() . "." . "$extension[1]";
-        $uploadDir = '../public/uploads/';
-        $uploadFile = $uploadDir . $filename;
-        move_uploaded_file($_FILES['avatar']['tmp_name'], $uploadFile);
-    } else {
-        $errors = "Vous ne pouvez uploader que des jpg, png ou gif";
+if (!empty($_FILES['images']['name'][0])) {
+    $files = $_FILES['images'];
+    $uploaded = [];
+    $errors = [];
+    $allowed = ['jpg','png','gif', 'jpeg'];
+
+    foreach ($files['name'] as $position => $fileName) {
+        $fileTmp = $files['tmp_name'][$position];
+        $fileSize = $files['size'][$position];
+        $fileError = $files['error'][$position];
+        $ext = explode('.', $fileName);
+        $ext = strtolower(end($ext));
+
+        if (in_array($ext, $allowed)) {
+            if ($fileError === 0) {
+                if ($fileSize <= 1000000) {
+                    $fileNameNew = uniqid('', true) . '.' . $ext;
+                    $fileDestination = __DIR__ . '/uploads/' . $fileNameNew;
+
+                    if (move_uploaded_file($fileTmp, $fileDestination)) {
+                        $uploaded[$position] = $fileDestination;
+                        header('Location : upload.php');
+                    } else {
+                        $errors[$position] = "[{$fileName}] n'a pas pu être uploadé";
+                    }
+
+                } else {
+                    $errors[$position] = "[{$fileName}] est trop lourd (1Mo max)";
+                }
+
+            } else {
+                $errors[$position] = "Un problème est survenu";
+            }
+
+        } else {
+            $errors[$position] = "Seuls les jpg, png et gif sont acceptés";
+            }
+        }
+
+    if (!empty($uploaded)) {
+        print_r($uploaded);
     }
-
+    if (!empty($errors)) {
+        print_r($errors);
+    }
 }
-/*$it = new FilesystemIterator("../public/uploads"(__FILE__));
-foreach ($it as $fileinfo) {
-    echo $fileinfo->getFilename() . "\n";
-}
-var_dump($it);
-*/
 
 ?>
+
 
 <!DOCTYPE html>
 <html>
@@ -35,15 +63,19 @@ var_dump($it);
 <form action="upload.php" method="post" enctype="multipart/form-data">
     <div class="form-group">
         <label for="imageUpload">Upload an profile image</label>
-        <input type="file" class="form-control-file" name="avatar" id="imageUpload" multiple>
+        <input type="hidden" name="MAX_FILE_SIZE" value="1000000">
+        <input type="file" class="form-control-file" name="images[]" id="imageUpload" multiple="multiple">
         <button type="submit">Send</button>
-        <?php if (!empty($errors)):?>
-        <?php foreach ($errors as $error)?>
-        <span><?= $error ?></span>
-            <?php endif?>
     </div>
 </form>
 
+
+<?php foreach ($it as $fileInfo): ?>
+    <figure>
+        <img src="uploads/<?= $fileInfo->getFilename()?>" alt="<?= $fileInfo->getFilename()?>" style="width: 300px">
+        <figcaption><?= $fileInfo->getFilename() . "\n" ?></figcaption>
+    </figure>
+<?php endforeach; ?>
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
